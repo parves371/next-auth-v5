8bdf12dd28dae786793bb0269e1bcf2d4d5e9674
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2024-10-29 12:45:10 +0600

    add Create user funtionality

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,11 @@
 "use server";
+
 import * as z from "zod";
+import becrypt from "bcrypt";
+
 import { Registerchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof Registerchema>) => {
   const validatedFeilds = Registerchema.safeParse(values);
@@ -10,7 +15,27 @@ export const register = async (values: z.infer<typeof Registerchema>) => {
     };
   }
 
+  const { email, password, name } = validatedFeilds.data;
+  const hashedPassword = await becrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return {
+      error: "Email already in use!",
+    };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+  // sent varification email
+
   return {
-    success: "Email sent!",
+    success: "User created successfully!",
   };
 };
